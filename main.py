7713a1fb2eafd22d9e6b9ddd017a126972935b40
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2018-04-06 21:11:00 +0200

    Initial Test --learning-rate 5

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+print("Begin!")
+
+print(sys.argv)
+
+assert sys.argv[1] == "--learning-rate"
+assert sys.argv[2] == "5"
+print("Passed!")
+
+assert sys.argv[2] == "5"
+
+import os.path
+
+assert os.path.exists('/data/check.txt')
+
+print("End!")
